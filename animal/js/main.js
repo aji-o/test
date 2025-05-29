fef97739340d401fe0b5238ac110a59c9f1bdea8
aji-o <ktu1019@gmail.com>
2025-05-29 16:22:21 +0900

    250422 동물보호센터 폰

--- animal/js/main.js
+++ animal/js/main.js
@@ -188,9 +188,63 @@ $(document).ready(function(){
         },
     });
 
+    /******************************** adopt - swiper 끝 ***********************************/
 
 
 
+    /***** - review - swiper 시작 *****/
+
+    const review_swiper = new Swiper('.review .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+
+            1024: {    /* 640px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        // },
+        navigation: {
+            nextEl: '.review .list .btn_next',
+            prevEl: '.review .list .btn_prev',
+        },
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+        },
+    });
+    // review_swiper.autoplay.stop();  /* 일시정지 기능 */
+    // review_swiper.autoplay.start();  /* 재생 기능 */
+
+        /***** - review - swiper 끝 *****/
+
+
+
+
+    /******************************** footer - top 시작 ***********************************/
+
+    $('footer .top2').on('click', function(){
+        console.log('top 클릭')
+        //$(window).scrollTop(0)
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500)
+    })
+
+
+
+    /******************************** footer - top 끝 ***********************************/
 
 
 }) // $(document).ready
