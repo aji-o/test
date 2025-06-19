e1d084ecd4b9b9427cbbb8d3af143d5b3b868308
aji-o <ktu1019@gmail.com>
2025-06-19 17:00:15 +0900

    250619

--- forest/js/main.js
+++ forest/js/main.js
@@ -31,10 +31,14 @@ $(document).ready(function(){
             }else if(destination.index == 3){
                 //console.log('4번째 슬라이드가 로딩 되었을때');
                 $('body').addClass('bg_white')
+            }else if(destination.index == 4){
+                //console.log('5번째 슬라이드가 로딩 되었을때' ///footer);
+                $('body').removeClass('bg_white')
             }
+            
         },
 
-        responsiveWidth: 1025, /* fullpage를 적용시키지 않을 모바일 사이즈 (1025부터 모바일) */
+        responsiveWidth: 769, /* fullpage를 적용시키지 않을 모바일 사이즈 (1025부터 모바일) */
         responsiveHeight: 700 /* 브라우저 높이가 700이하로 줄면 fullpage 안함 */
     }); // 풀페이지 종료
 
@@ -95,5 +99,29 @@ $(document).ready(function(){
 
     });
 
+
+
+    /*#####################    story swiper     ########################*/
     
+    const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            // 449: {    /* 640px 이상일때 적용 */
+            //     slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+            //     spaceBetween: 16,
+            // },
+            769: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+            1281: {    /* 640px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    });
+
 })
