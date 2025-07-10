66a33aac8516dbfcdde57d3715bd07f1020be09a
aji-o <ktu1019@gmail.com>
2025-07-10 17:59:11 +0900

    250710 hyo

--- hyosung/js/main.js
+++ hyosung/js/main.js
@@ -23,8 +23,7 @@ $(document).ready(function(){
 
     /* ################### visual ####################### */
 
-    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-
+    const visual_swiper = new Swiper('.visual .swiper', { 
         autoplay: {
             delay: 5000,
             disableOnInteraction: true,
@@ -32,21 +31,13 @@ $(document).ready(function(){
 
         loop: true,
 
-        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.visual .ctrl_wrap .count', /* 해당 요소의 class명 */
+        pagination: {
+            el: '.visual .ctrl_wrap .count',
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
             type: 'fraction',
         },
 
-        // navigation: {  /* 이전, 다음 버튼 */
-        //     nextEl: '.swiper-button-next',  /* 다음 버튼의 클래스명 */
-        //     prevEl: '.swiper-button-prev',  
-        // },
-
     });
-
-    // visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-    // visual_swiper.autoplay.start();  /* 재생 기능 */
 
     $('.visual .ctrl_wrap button.button-stop').on('click', function(){
         visual_swiper.autoplay.stop(); /* 일시정지 */
@@ -62,24 +53,50 @@ $(document).ready(function(){
 
     /* #################  center  ####################*/
 
-    const center_swiper = new Swiper('.center .swiper', { /* 팝업을 감싼는 요소의 class명 */
-	slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+    const center_swiper = new Swiper('.center .swiper', { 
+	slidesPerView: 3, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
 	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
 	breakpoints: {
-		768: {    /* 768px 이상일때 적용 */
-			slidesPerView: 5,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+		769: {
+			slidesPerView: 3,    /* 'auto' 라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+			spaceBetween: 24,
+		},
+        1025: {
+			slidesPerView: 4,
 			spaceBetween: 24,
 		},
 	},
-	centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-	// autoplay: {  /* 팝업 자동 실행 */
-	// 	delay: 2500,
-	// 	disableOnInteraction: true,
-	// },
+	centeredSlides: true,
+	loop: true,
+
 
 });
-// swiper.autoplay.stop();  /* 일시정지 기능 */
-// swiper.autoplay.start();  /* 재생 기능 */
+
+    /* #################  news  ####################*/
+
+    const news_swiper = new Swiper('.news .swiper', {
+
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: true,
+        },
+
+        //effect: "fade", /* fade 효과 */
+
+        loop: true,
+
+        pagination: {
+            el: '.news .paging',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.news .btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.news .btn_prev',  
+        },
+    });
+
+
+
+
 
 })
