b1fa45459ee717a15c51547c3fce7a72b81fa886
aji-o <ktu1019@gmail.com>
2025-07-24 01:20:01 +0900

    250724 po

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -5,9 +5,29 @@ $(document).ready(function(){
         mousewheel: true,
         on: {
             slideChange: function(){
-                $('header ul li').removeClass('active')
-                $('header ul li').eq(this.realIndex).addClass('active')
-            },
+                $('header ul li').removeClass('active');
+                $('header ul li').eq(this.realIndex).addClass('active');
+            }
+        }
+    });
+
+    // gnb 클릭 시 swiper 이동
+    const sectionMap = {
+        'home': 0,
+        'profile': 1,
+        'hollys': 2,
+        'hyoseng': 3,
+        'nest': 4,
+        'contact': 5
+    };
+
+    $('header ul li a').on('click', function(e){
+        e.preventDefault();
+        const target = $(this).attr('href').replace('#', ''); 
+        const index = sectionMap[target];
+
+        if (index !== undefined) {
+            swiper.slideTo(index); // Swiper 슬라이드 이동
         }
     });
 });
