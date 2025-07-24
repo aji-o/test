e50d9ecfb47f9f371605178a254b8b62ac0d4db6
aji-o <ktu1019@gmail.com>
2025-07-24 12:40:49 +0900

    250724

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -30,4 +30,15 @@ $(document).ready(function(){
             swiper.slideTo(index); // Swiper 슬라이드 이동
         }
     });
+
+        // 마우스 커서
+
+        $(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
+        $('.cursor').css('left', e.pageX + 'px');
+        $('.cursor').css('top', e.pageY + 'px');
+    });
+    $('.hollys .tit a, .hyoseng .tit a, .nest .tit a, .hollys a.bg_btn, .hyoseng a.bg_btn, .nest a.bg_btn').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+        $('.cursor').toggleClass('on');
+    });
+
 });
